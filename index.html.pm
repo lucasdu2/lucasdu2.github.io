7c14ea135a50d0af33301cfc8e3a314eab2ba3be
Lucas Du <lucaszdu@gmail.com>
2024-06-28 12:47:23 -0400

    Procrastinate by fiddling around with text content

--- index.html.pm
+++ index.html.pm
@@ -5,18 +5,24 @@
 
 ◊headline{About}
 
-◊portrait["assets/myface.jpg" "a picture of my face"] I am an MS student at UC Davis studying computer science. Previously, I was a Technical Consultant at Hitachi Vantara, working on cloud storage at eBay. Even before that, I graduated from the University of Chicago (BS '21) with a degree in computer science and a minor in English and creative writing.
+◊portrait["assets/myface.jpg" "a picture of my face"] I am an MS student at UC Davis studying computer science.
 
-I'm interested in ◊strong{programming languages} and ◊strong{formal methods research} to both ◊strong{improve the quality of software systems} and ◊strong{make it easier to develop higher quality software}. More broadly, I'm interested in moving software development away from heuristics and hacks and onto solid, principled ground.
+Previously, I was a Technical Consultant at Hitachi Vantara, working on cloud storage at eBay. Even before that, I graduated from the University of Chicago (BS '21) with a degree in computer science and a minor in English and creative writing.
+
+I'm interested in ◊strong{programming languages} and ◊strong{formal methods research} to ◊strong{improve the quality of software systems} and ◊strong{make it easier to develop higher quality software}. Software should be more empowering and less frustrating for both developers and users.
+
+More broadly, I'm interested in moving software development away from heuristics and hacks and onto more solid, elegant, and principled ground. I think there is a lot of room for mathematical abstractions to give us much better results across the computing stack.
 
 ◊headline{Research Interests}
-I'm primarily interested in ◊strong{applying techniques from programming languages and formal methods to software systems}. My interests within this umbrella are fairly varied, but I'm currently excited about ◊strong{verification of concurrent/distributed systems} and ◊strong{better programming models for the cloud}, particularly related to distributed storage and stream processing.
+I'm primarily interested in ◊strong{applying techniques from programming languages and formal methods to software systems}. I'm currently excited about:
+◊items{
+  ◊item{◊strong{verification of concurrent/distributed systems} and}
+  ◊item{◊strong{better programming models for the cloud}, particularly related to distributed storage and stream processing.}
+}
 
-I'm also generally interested in compilers, type systems, web browsers, scientific and high performance computing, security and cryptography, peer-to-peer and decentralized systems, data privacy, program synthesis, and so on. But life is finite. I do hope that anything I do with computers centers on some subset of these interests.
-
-At a much higher and more idealistic level, I'm interested in software that is ◊strong{more empowering} and ◊strong{less exploitative, extractive, or otherwise harmful}. I want software that adds more to our lives and our world than it takes away. 
+I'm also generally interested in compilers, type systems, web browsers, scientific and high performance computing, databases, security and cryptography, peer-to-peer and decentralized systems, data privacy, program synthesis, and so on. But life is finite. I do hope that anything I do with computers centers on some subset of these interests.
 
 ◊headline{Personal}
-I like reading short stories, writing fiction, and listening to music. I also like long form journalism and literary criticism. At one point I wanted to be a journalist. At another point, I wanted to be a writer. I like a lot of humanities-related things and it's unfortunate that market forces have deemed most of them to be strictly less valuable than producing software.
+I like reading short stories, writing fiction, and listening to music. I also like long form journalism and literary criticism. At one point, I wanted to be a journalist. At another point, I wanted to be a writer. I like a lot of humanities-related things and it's unfortunate that market forces have deemed most of them to be strictly less valuable than producing software.
 
 ◊footer{made with robotic indifference by yours truly, with help from ◊link["https://docs.racket-lang.org/pollen/index.html"]{pollen}}
