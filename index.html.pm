7f1c7db5b701099139de1063773f466c6ca0db6c
Lucas Du <lucaszdu@gmail.com>
2024-07-05 22:18:03 -0400

    Quick restyle

--- index.html.pm
+++ index.html.pm
@@ -1,17 +1,16 @@
 #lang pollen
 
 ◊header{◊title{Lucas Du}◊nav{◊link["index.html"]{Home}◊link["writing.html"]{Writing}}}
-◊email{◊em{lucaszdu} [at] ◊em{gmail}} | ◊link["https://github.com/lucasdu2"]{github} / ◊link["https://linkedin.com/in/lucaszdu"]{linkedin}
 
 ◊headline{About}
 
-◊portrait["assets/myface.jpg" "a picture of my face"] I am an MS student at UC Davis studying computer science.
+◊portrait["assets/myface.jpg" "a picture of my face"] I am an ◊strong{incoming Computer Science MS student at UC Davis}. I'm currently taking a brief break between working full-time and starting school.
 
 Previously, I was a Technical Consultant at Hitachi Vantara, working on cloud storage at eBay. Even before that, I graduated from the University of Chicago (BS '21) with a degree in computer science and a minor in English and creative writing.
 
-I'm interested in ◊strong{programming languages} and ◊strong{formal methods research} to ◊strong{improve the quality of software systems} and ◊strong{make it easier to develop higher quality software}. Software should be more empowering and less frustrating for both developers and users.
+I'm interested in ◊strong{programming languages} and ◊strong{formal methods research} to ◊em{improve} the quality of software systems and ◊em{make it easier} to develop higher quality software. Software should be more empowering and less frustrating for both developers and users.
 
-More broadly, I'm interested in moving software development away from heuristics and hacks and onto more solid, elegant, and principled ground. I think there is a lot of room for mathematical abstractions to give us much better results across the computing stack.
+More broadly, I'm interested in moving software away from heuristics and hacks and onto more solid, elegant, and principled ground. I think there is a lot of room for mathematical abstractions to give us much better results across the computing stack. And as software worms its way into more of our lives, we need better results.
 
 ◊headline{Research Interests}
 I'm primarily interested in ◊strong{applying techniques from programming languages and formal methods to software systems}. I'm currently excited about:
@@ -20,9 +19,7 @@ I'm primarily interested in ◊strong{applying techniques from programming langu
   ◊item{◊strong{better programming models for the cloud}, particularly related to distributed storage and stream processing.}
 }
 
-I'm also generally interested in functional programming, compilers, type systems, web browsers, scientific and high performance computing, databases, security and cryptography, peer-to-peer and decentralized systems, data privacy, program synthesis, and so on. But life is finite. I do hope that anything I do with computers centers on some subset of these interests.
-
 ◊headline{Personal}
-I like reading short stories, writing fiction, and listening to music. I also like long form journalism and literary criticism. At one point, I wanted to be a journalist. At another point, I wanted to be a writer. I like a lot of humanities-related things and it's unfortunate that market forces have deemed most of them to be strictly less valuable than producing software.
+I like reading short stories, writing fiction, and listening to music. I also like long form journalism and literary criticism. At one point, I wanted to be a journalist. At another point, I wanted to be a writer. I generally like a lot of humanities-related things and it's unfortunate that market forces have deemed most of them to be strictly less valuable than producing software.
 
 ◊footer{made with robotic indifference by yours truly, with help from ◊link["https://docs.racket-lang.org/pollen/index.html"]{pollen}}
--- styles.css.pp
+++ styles.css.pp
@@ -3,12 +3,12 @@
 ◊(define inner 2)
 ◊(define edge (* inner 2))
 ◊(define fontsize 16)
-◊(define bgcolor "#FEFBEA")
-◊(define highlightcolor "#fcf1b6")
+◊(define bgcolor "#FAF9F6")
+◊(define highlightcolor "#FEFBEA")
 ◊(define calloutcolor "#8b0000")
 ◊(define dividercolor "#D3D3D3")
 ◊(define bodywidth 40)
-◊(define h1-ratio 2.3)
+◊(define h1-ratio 2)
 ◊(define h2-ratio 1.5)
 ◊(define footer-ratio 0.7)
 ◊(define multiplier 1.3)
@@ -39,7 +39,6 @@ html {
   font-family: "Inria Sans", sans-serif;
   display: table;
   margin: auto;
-  background: ◊|bgcolor|;
 }
 
 header {
@@ -47,38 +46,39 @@ header {
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
+  background: linear-gradient(90deg, RGBA(185, 189, 134, 1), RGBA(185, 189, 134, 0.5));
+  border-top: 2px solid #535724;
+  border-bottom: 2px solid #535724;
   gap: 10%;
 }
 
 header nav {
   display: flex;
-  align-content: end;
-  align-items: end;
-  font-size: 1.3em;
-  margin-bottom: 5px;
+  align-items: start;
+  font-size: 1em;
   gap: 0.5em;
+  margin-right: 0.5em;
 }
 
 header nav a {
+  color: ◊|bgcolor|;
   text-decoration: none;
-  font-weight: bold;
-  padding: 0.1em 0.2em;
+  padding: 0.2em 0.4em;
+  background-color: #535724;
 }
 
 header nav a:hover {
-  text-decoration: none;
-  background-color: ◊|highlightcolor|;
+  text-decoration: underline;
 }
 
 a:link, a:visited {
-  color: ◊|calloutcolor|;
 }
 
 .portrait {
   width: 15em;
-  border-radius: 50%;
+  border-radius: 30%;
   float: right;
-  margin: 0.7em;
+  margin: 1em;
   border: 3px solid ◊|dividercolor|;
 }
 
@@ -95,11 +95,13 @@ body {
 h1 {
   font-size: ◊|h1-ratio|em;
   margin: 0em;
+  padding-left: 0.2em;
 }
 
 h2 {
   font-size: ◊|h2-ratio|em;
   padding-left: 0.2em;
+  padding-top: 0.1em;
   background: linear-gradient(90deg, rgba(195, 177, 225,1) 0%, rgba(195, 177, 225,0.2) 100%);
   border-left: 1px solid #800080;
   border-bottom: 1px solid #800080;
@@ -117,8 +119,5 @@ footer {
       display: block;
       margin-left: auto;
       margin-right: auto;
-  }
-  header nav a {
-    padding: 0;
   }
 }
