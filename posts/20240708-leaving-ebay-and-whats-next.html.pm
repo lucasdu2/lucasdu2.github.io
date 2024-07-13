dd569a7444ba71b3fd61f5d141b722be4e6b1c4a
Lucas Du <lucaszdu@gmail.com>
2024-07-13 18:40:20 -0400

    Finish post content

--- posts/20240708-leaving-ebay-and-whats-next.html.pm
+++ posts/20240708-leaving-ebay-and-whats-next.html.pm
@@ -3,18 +3,33 @@
 ◊post-title{Leaving eBay (and what's next)}
 ◊post-date{Monday July 8, 2024}
 
-I officially left my full-time job at eBay last Friday! It's a weird and exciting time. And I have a lot of thoughts (probably too many).
+I officially left my full-time job at eBay last Friday! I'll be starting an MS program at UC Davis, probably at the beginning of 2025. It's a weird and exciting time.
 
-I think it would be fun and good for me to start writing regularly, at least in this interstitial period between working and starting up school again. Most, if not all, the things I write about here will probably be related to programming languages, verification, software engineering, and computing more broadly.
-
-I care about a number of other things outside of that umbrella, but I'd prefer to keep those parts of my life separate, at least online. For better or worse, I spend the bulk of my time these days doing programming and computer science things anyway.
-
-◊highlight['python #:python-executable "python3"]{
-for x in range(3):
-    print x
-}
+I think it would possibly be fun for me to start writing regularly, at least in this interstitial period between working and starting up school again. Most, if not all, the things I write about here will probably be related to programming languages, verification, software engineering, and computing more broadly. I also might not write anything at all. Who knows what the future holds?
 
 ◊post-section{How I'm feeling now}
+I think most modern software is in a terrible state. This is in no way an indictment of eBay or anyone I worked with --- I actually feel like I had a really great experience doing cloud storage at eBay, at least for a first job out of college, as someone without much in the way of real world programming experience. I learned a ton. The people I worked with were incredibly smart and supportive. Everyone was doing the best they could with what they had at hand.
+
+But for me, over the past couple years in industry, I've become increasingly dissatisfied with the status quo. Most of the tools we work with, the software we work with, are massive agglutinations of hacks, patches, and historical accident. A lot of modern software development is comprised of meaningless tasks --- ◊em{accidental} complexity, in the words of Fred Brooks --- that, in my view, are a direct result of an industry fetishization of ad hoc solutions, "velocity", and profit-chasing. There's only so far we can take this approach before we need to start taking a hard, principled look at the roots of our problems. At least that's what I believe. 
+
+A common argument from an engineering perspective is that "things just work." Who cares if it's cobbled together and built on sand! But I really don't think that's true. Things ◊em{don't} work. Not really. Software is often brittle, incorrect, laden with bugs that cause security issues and privacy leaks, and unacceptably slow.
+
+We live in a world where no one really knows what's going on with their systems and no one can really trust that their software won't fail them. I don't feel comfortable living in a world where ambitious individuals, intent on "disruption" and extracting ever-more monetary value from people, plow ahead with trying to base larger parts of our society on software when we can't even trust our software to work well.
+
 ◊post-section{A bit of self-reflection}
+While I do think the state of software isn't great, that isn't the only reason why I'm currently motivated to leave industry. I also just miss school and, more broadly, the academic atmosphere. I want to have more time and space to learn things that truly interest me and think hard about problems that don't have quick and easy solutions and that don't have direct business value. I want to do things that are more meaningful to me.
+
+I'm also just really intellectually interested in a lot of the academic work around programming languages, formal methods, and systems. I want to spend more of my time engaging with that research and with those ideas. And I think graduate school is a really good way to get to do that. The fact that I believe this work is actually quite important is just the icing on the cake.
+
+That's not to say that I don't see value in the software industry. I think there are certain things that can only be done, certain kinds of real-world impact that can only be achieved, in a for-profit setting. And there are software firms out there that are doing things I believe in and that care deeply about quality and craft. I wouldn't preclude a return to commercial work. Just not at the moment.
 
 ◊post-section{What I want to do}
+I want to work on better tools and abstractions to broadly improve our software --- it should be easier to create software that is more correct, more secure, more private, more performant, and more flexible. We need better ways of wrangling ballooning complexity and developing more trustworthy, robust systems. 
+
+I'm interested in programming languages research, primarily because I think it offers a rigorous, principled, and foundational way to think about a lot of these problems. I think math and logic have a lot to say when it comes to better ways of building and growing software.
+
+I think the best way to do all of this is to go to graduate school. I simply don't think there are many places in industry that are accessible to me --- at least, at the moment --- that are working on these problems in ways that I find satisfying. There certainly aren't many places in industry that offer the degree of intellectual freedom that the academy does. There's more tolerance for uncertainty in academia. There's more leeway to take big swings. I want to take some big swings.
+
+We'll see what happens though. There are lots of things that aren't so nice about academia too --- whether its toxic power dynamics, politics, or the incessant pressure to publish. And of course, as in any other highly competitive field, a path in academia isn't guaranteed (to say the least). That honestly might be one of the hardest realizations of growing up: sometimes, no matter how hard you try, you won't succeed.
+
+But it doesn't really matter. If you truly want it, you still have to give it your best shot.
