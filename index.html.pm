92a8cac8f9c1a95347d91c60f895e416504e6d7e
Lucas Du <lucaszdu@gmail.com>
2024-07-19 01:05:14 -0400

    Final edits (for now) to research interests

--- index.html.pm
+++ index.html.pm
@@ -11,17 +11,13 @@ I'm interested in ◊strong{programming languages} and ◊strong{formal methods 
 ◊email{lzdu ∊ ucdavis} ∨ ◊email{lucaszdu ∊ gmail} / ◊link["https://github.com/lucasdu2"]{GitHub} / ◊link["https://linkedin.com/in/lucaszdu"]{LinkedIn}
 
 ◊headline{Research Interests}
-I'm primarily interested in ◊strong{applying techniques from programming languages and formal methods to software systems}. ◊em{It should be easier to create software that is more correct, performant, secure, private, and flexible}. I'm currently excited about:
-◊items{
-  ◊item{◊strong{verification of concurrent/distributed systems} and}
-  ◊item{◊strong{better programming models for the cloud}, particularly related to distributed storage and stream processing.}
-}
+I'm primarily interested in research at the intersection of ◊strong{programming languages}, ◊strong{formal methods}, and ◊strong{systems}. It should be easier to create software --- particularly concurrent and distributed software --- that is more correct, efficient, secure, private, and flexible. Programming languages and formal methods offer a theoretically rich and principled place to start.
+
+Currently, I'm interested in the formal verification of concurrent and distributed systems. I want to prove things about programs. I'm also interested in better programming models for parallel and distributed computing.
 
 ◊|divider|
 
-One formulation of my larger goal is to take a mathematically principled approach in A) expanding and strengthening the kinds of formal guarantees we can practically get about our software and B) discovering simpler, more expressive programming tools and abstractions for contemporary computing needs.
-
-More broadly, I want to help move software ◊em{away from heuristics and hacks} and ◊em{onto more solid, elegant, and principled ground}. I believe there is a lot of room for mathematical abstractions to give us much better results across the computing stack. As software underpins more and more of our lives, we need better results.
+More broadly, I want to help move software ◊em{away from heuristics and hacks} and ◊em{onto more solid, elegant, and principled ground}. I believe there is a lot of room for mathematical abstractions to give us much better results across the computing stack. And as software underpins more and more of our lives, we need better results.
 
 ◊headline{Personal}
 I like reading short stories, writing fiction, and listening to music. I also like long form journalism and literary criticism. At one point, I wanted to be a journalist. At another point, I wanted to be a writer. I generally like a lot of humanities-related things and it's unfortunate that market forces have deemed most of them to be strictly less valuable than producing software.
