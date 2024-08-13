40a2f44ed9ff16452afb98dd339d9a207649fdb4
Lucas Du <lucaszdu@gmail.com>
2024-08-13 16:03:52 -0400

    A bit more fussing

--- index.html.pm
+++ index.html.pm
@@ -9,12 +9,12 @@ Previously, I worked on cloud storage at eBay. As an undergraduate, I studied co
 I'm interested in ◊strong{programming languages} and ◊strong{formal methods research} to ◊em{improve} the quality of software systems and ◊em{make it easier} to develop higher quality software. Software should be more empowering and less frustrating for both developers and users.
 
 ◊headline{Research Interests}
-Currently, I'm interested in the ◊strong{formal verification of concurrent and distributed systems}. I want strong formal guarantees, specifically about correctness and security, from foundational systems (i.e. databases, filesystems, browsers) and web-based applications.
+Currently, I'm interested in the ◊strong{formal verification of concurrent and distributed systems}. I want strong formal guarantees, specifically about correctness and security, from performance-sensitive foundational systems (i.e. databases, filesystems, browsers) and web-based applications.
 
 I'm also interested in ◊strong{better programming models for parallel and distributed computing}. We need better tools and abstractions to make building simple and efficient (and correct, secure, and private) parallel/concurrent/distributed software much easier.
 
 ◊|divider|
 
-Broadly, I believe practical software development can be much more rigorous and elegant. Programming languages and formal methods offer a theoretically rich and principled place to start. There is a lot of room for mathematical abstractions to give us much better results across the computing stack and as software underpins more and more of our lives, ◊em{we need better results}.
+Broadly, I believe practical software development can be much more rigorous and elegant. Programming languages and formal methods offer a theoretically rich and principled place to start. There is a lot of room for mathematical abstractions to give us far better results across the computing stack and as software underpins more and more of our lives, ◊em{we need better results}.
 
 ◊footer{made with robotic indifference by yours truly, with help from ◊link["https://docs.racket-lang.org/pollen/index.html"]{pollen}}
