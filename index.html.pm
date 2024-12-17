38dbca0f491dcce29bdf46374453245a1479fb63
Lucas Du <lucaszdu@gmail.com>
2024-12-17 01:14:43 -0500

    Shorten footer text

--- index.html.pm
+++ index.html.pm
@@ -4,7 +4,7 @@
 
 I'm interested in ◊strong{programming languages} and ◊strong{formal methods}, with an eye to increasing the ◊em{correctness, security, privacy, and simplicity} of realistic software systems. I also have a particular interest in concurrent and distributed systems. More broadly, I want to help move software ◊em{away from heuristics and hacks} and ◊em{onto more solid, elegant, and principled ground}.
 
-◊footer{made with robotic indifference by yours truly, with help from ◊link["https://docs.racket-lang.org/pollen/index.html"]{pollen}
+◊footer{made with robotic indifference, with help from ◊link["https://docs.racket-lang.org/pollen/index.html"]{pollen}
 lzdu ∊ ucdavis / ◊link["https://github.com/lucasdu2"]{GitHub} / ◊link["https://linkedin.com/in/lucaszdu"]{LinkedIn}
 
 ◊link["https://www.recurse.com/scout/click?t=ff3a1c13d455733bc31c3dd5ec3d1cf4"]{◊image["assets/rc-icon.png" "Recurse Center logo" "20em"]}}
