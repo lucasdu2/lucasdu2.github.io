324691adebd9052e319653d49ee76d1a28816ba9
Lucas Du <lucaszdu@gmail.com>
2025-03-05 08:57:52 -0800

    Minor wording changes

--- index.html.pm
+++ index.html.pm
@@ -2,7 +2,7 @@
 
 ◊portrait["assets/myface.jpg" "a picture of my face"] I am a computer science MS student at ◊link["https://cs.ucdavis.edu/graduate/graduate-group"]{UC Davis}. Previously, I worked on cloud storage at ◊link["https://innovation.ebayinc.com/tech/engineering/"]{eBay}. As an undergraduate, I studied computer science at ◊link["https://cs.uchicago.edu/"]{UChicago}.
 
-I'm interested in ◊strong{programming languages} and ◊strong{formal methods}, with an eye to increasing the ◊em{correctness, security, privacy, and simplicity} of realistic software systems in ◊em{provable} ways. I also have a particular interest in concurrent and distributed systems. More broadly, I want to help move software ◊em{away from heuristics and hacks} and ◊em{onto more solid, elegant, and principled ground}.
+I'm interested in ◊strong{programming languages} and ◊strong{formal methods}, with an eye to increasing the ◊em{correctness, security, privacy, and simplicity} of realistic software systems in ◊em{provable and foundational} ways. I also have a particular interest in concurrent and distributed systems. More broadly, I want to help move software ◊em{away from heuristics and hacks} and ◊em{onto more solid, elegant, and principled ground}.
 
 ◊footer{made with robotic indifference, with help from ◊link["https://docs.racket-lang.org/pollen/index.html"]{pollen}
 lzdu ∊ ucdavis / ◊link["https://github.com/lucasdu2"]{GitHub} / ◊link["https://linkedin.com/in/lucaszdu"]{LinkedIn}
