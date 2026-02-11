c1648e8868ed1326b4f4855f6f9b3888684c3492
Lucas Du <lucaszdu@gmail.com>
2026-02-11 08:58:12 -0800

    Update with some academic service and activities

--- index.html.pm
+++ index.html.pm
@@ -4,6 +4,10 @@
 
 I like ◊strong{programming languages}, ◊strong{formal methods}, and applications of techniques from these areas to (concurrency ∪ distribution) and (security ∪ privacy). I aim to develop mathematically principled, language-oriented tools and abstractions for building ◊em{provably correct}, ◊em{efficient}, ◊em{flexible}, and ◊em{simple} software, preferably ◊em{by-construction}.
 
+◊small-section{Service and Activities}
+◊strong{Service:} OOPSLA 2026 AEC
+◊strong{Activities:} OPLSS 2025, SPLV 2025
+
 ◊small-section{Miscellaneous}
 ◊id-table{◊item-description[◊link["files/language-ifc-survey-ecs235b.pdf"]{A Survey of Language-Based Information Flow Control}]{A brief and limited survey containing: a basic introduction to the theory of information flow control; some analysis of attempts to integrate IFC techniques into existing programming languages and language ecosystems; some thoughts on language features that enable such integration, particularly of static, type-based IFC. Final project for ECS235B at UC Davis (Spring 2025).}◊item-description[◊link["files/milner-processes.pdf"]{"Processes: A Mathematical Model of Computing Agents" (Milner 1973)}]{A scan of Robin Milner's paper, which appeared in the Proceedings of the Logic Colloqium (Bristol, July 1973). It contains some early ideas of what would later become CCS and the pi-calculus. I wasn't able to find a copy online anywhere else, so here it is!}}
 
